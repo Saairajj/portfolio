bc722e8b0ee27022a0ef7e58c9f7decabe905b4b
Sairaj Jadhav <saairajj812@gmail.com>
2020-07-27 03:29:30 +0530

    Updates

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Testing");
+
+let themeDot = document.getElementsByClassName('theme-dot')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null) {
+  setTheme('light')
+}else{
+  setTheme(theme)
+}
+
+for(var i=0; i<themeDot.length; i++) {
+  themeDot[i].addEventListener('click', function() {
+    let mode = this.dataset.mode
+    console.log("Cilicked : " + mode);
+    setTheme(mode)
+  })
+}
+
+function setTheme(mode) {
+  if(mode == 'light') {
+    document.getElementById('theme-style').href = 'style.css'
+  }
+  if(mode == 'blue') {
+    document.getElementById('theme-style').href = 'blue.css'
+  }
+  if(mode == 'green') {
+    document.getElementById('theme-style').href = 'green.css'
+  }
+  if(mode == 'purple') {
+    document.getElementById('theme-style').href = 'purple.css'
+  }
+
+  localStorage.setItem('theme', mode)
+}
